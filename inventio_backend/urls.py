82f764a45e0dd1d8cce822004c3abd01ea4508f6
Jakub Kusal <kusal.jakub@wp.pl>
2025-03-29 21:35:03 +0100

    feat: Created db connections and models (#9)

--- inventio_backend/urls.py
+++ inventio_backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for InventioBackend project.
+URL configuration for inventio_backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InventioBackend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventio_backend.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
