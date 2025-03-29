99e8dc2caba87f80b058cd20ae54996ed5cad653
Jakub Kusal <kusal.jakub@wp.pl>
2025-03-29 21:25:40 +0100

    Removed commented shit

--- stocktaking/models.py
+++ stocktaking/models.py
@@ -12,15 +12,3 @@ class Stocktaking(models.Model):
     device_id = models.ForeignKey(Device, on_delete=models.CASCADE)
     released_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="released_by_stocktaking")
     returned_by = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="returned_by_stocktaking")
-
-
-# class Issue(models.Model):
-#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     closed_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-#     closed_by = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-#     topic = models.CharField(max_length=128)
-#     description = models.TextField()
-#     note = models.CharField(max_length=128, blank=True)
-#     image = models.ImageField(blank=True, null=True)
-#     device_id = models.ForeignKey(Device, on_delete=models.CASCADE)
