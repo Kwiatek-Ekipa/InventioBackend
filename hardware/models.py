46def4286da5762706f6bc39f013a2726019af4a
Jakub Kusal <kusal.jakub@wp.pl>
2025-03-29 21:02:19 +0100

    feat: Created db connections and

---
+++ hardware/models.py
@@ -0,0 +1,25 @@
+import uuid
+from django.core.validators import MinLengthValidator, MinValueValidator
+from django.db import models
+from inventio_auth.models import User
+
+
+class Brand(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=128, validators=[MinLengthValidator(2)])
+
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=128, validators=[MinLengthValidator(2)])
+
+
+class Device(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    brand_id = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    model = models.CharField(max_length=128)
+    year_of_production = models.IntegerField(validators=[MinValueValidator(1950)])
+    added_date = models.DateTimeField(auto_now_add=True)
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    serial_number = models.CharField(max_length=128)
--- inventio_backend/wsgi.py
+++ inventio_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for InventioBackend project.
+WSGI config for inventio_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InventioBackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventio_backend.settings')
 
 application = get_wsgi_application()
