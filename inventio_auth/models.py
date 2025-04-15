267044fb9178c67fd5af6f1ddb50ce7d6922baf2
Paweł Morawczyński <burciak01@gmail.com>
2025-04-15 18:40:28 +0200

    ref: replace literal role string on RoleEnum value

--- inventio_auth/models.py
+++ inventio_auth/models.py
@@ -3,9 +3,12 @@ from django.core.validators import MinLengthValidator
 from django.db import models
 import uuid
 
+from inventio_auth.enums import RoleEnum
+
+
 class Role(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=128, validators=[MinLengthValidator(2)])
+    name = models.CharField(max_length=128, choices=RoleEnum.choices(), default=RoleEnum.WORKER)
 
 
 class Account(AbstractBaseUser):
--- inventio_auth/views.py
+++ inventio_auth/views.py
@@ -1,6 +1,9 @@
 from rest_framework import status
-from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+
+from .enums import RoleEnum
 from .permissions import is_technician
 from .serializers import UserRegisterSerializer, UserInfoSerializer, CreateTechnicianSerializer
 from .models import Account
@@ -46,9 +49,9 @@ def delete_technician(request, user_id):
         return Response(status=status.HTTP_404_NOT_FOUND)
     if request.user == user_to_delete:
         return Response(status=status.HTTP_403_FORBIDDEN)
-    if user_to_delete.role.name != 'Technician':
+    if user_to_delete.role.name != RoleEnum.TECHNICIAN:
         return Response(status=status.HTTP_400_BAD_REQUEST)
-    technician_count = Account.objects.filter(role__name='Technician').count()
+    technician_count = Account.objects.filter(role__name=RoleEnum.TECHNICIAN).count()
     if technician_count <= 1:
         return Response(status=status.HTTP_400_BAD_REQUEST)
     user_to_delete.delete()
