9af06e6ac82ecbfb32adcbb395eccbe953963dd9
Paweł Morawczyński <burciak01@gmail.com>
2025-04-15 18:43:14 +0200

    ref: refactor

--- inventio_auth/urls.py
+++ inventio_auth/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path, include
-from . import views
+from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from . import views
 
 app_name = 'inventio_auth'
 
--- inventio_backend/settings.py
+++ inventio_backend/settings.py
@@ -144,8 +144,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=int(config('ACCESS_TOKEN_LIFETIME_MINUTES', default=60))),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=int(config('REFRESH_TOKEN_LIFETIME_DAYS', default=7))),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=config('ACCESS_TOKEN_LIFETIME_MINUTES', cast=int)),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=config('REFRESH_TOKEN_LIFETIME_DAYS', cast=int)),
 
 }
 
