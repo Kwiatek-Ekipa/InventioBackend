3caf6f38be0f2626b0b8e40d40b7a2411609afd2
Paweł Morawczyński <burciak01@gmail.com>
2025-04-21 00:53:37 +0200

    ref: IsTechnician and IsWorker permission classes added

--- inventio_backend/urls.py
+++ inventio_backend/urls.py
@@ -17,9 +17,6 @@ Including another URLconf
 # from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('api/auth/', include('inventio_auth.urls', namespace='inventio_auth')),
-
-
 ]
