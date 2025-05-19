0f837955af9197037fc7b9332c760055c13e64e5
Klusio19 <kusal.jakub@wp.pl>
2025-05-19 23:48:37 +0200

    ref: Consolidated seeders for auth.

--- inventio_auth/management/commands/seedauth.py
+++ inventio_auth/management/commands/seedauth.py
@@ -1,8 +1,9 @@
 from django.core.management import BaseCommand
-from inventio_auth.management.commands import seedroles, seedtechnician
+from inventio_auth.management.commands import seedroles, seedtechnicians, seedworkers
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         seedroles.Command().handle(args, options)
-        seedtechnician.Command().handle(args, options)
+        seedtechnicians.Command().handle(args, options)
+        seedworkers.Command().handle(args, options)
