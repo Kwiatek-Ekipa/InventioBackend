2c172e9e0f098931c2a44494f8d072c33aea568a
Klusio19 <kusal.jakub@wp.pl>
2025-05-20 13:40:01 +0200

    ref: Argument logic reversed.

--- inventio_auth/management/commands/seedauth.py
+++ inventio_auth/management/commands/seedauth.py
@@ -4,16 +4,15 @@ from django.core.management import BaseCommand, call_command
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument(
-            "--production",
+            "--devel",
             action="store_true"
         )
 
     def handle(self, *args, **options):
         call_command("seedroles")
 
-        if options["production"]:
-            call_command("seedtechnicians", "--production")
-
+        if options["devel"]:
+            call_command("seedworkers")
+            call_command("seedtechnicians", "--devel")
         else:
             call_command("seedtechnicians")
-            call_command("seedworkers")
