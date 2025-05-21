b79fcd593071f2fddc65fd9798ee24c6d717b5d7
Klusio19 <kusal.jakub@wp.pl>
2025-05-21 19:50:40 +0200

    ref: Model changes for date.

--- stocktaking/models.py
+++ stocktaking/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 import uuid
+from django.utils.timezone import now
+
 from inventio_auth.models import Account
 from hardware.models import Device
 
 
 class Stocktaking(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    release_date = models.DateTimeField(auto_now_add=True)
+    release_date = models.DateTimeField(default=now)
     return_date = models.DateTimeField(blank=True, null=True)
     user = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="user_stocktaking")
     device = models.ForeignKey(Device, on_delete=models.CASCADE)
