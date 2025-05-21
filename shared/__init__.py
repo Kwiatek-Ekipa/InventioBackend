c86c2ad8d1a3d787263cfc3a3f9e2a99d46c58f8
Paweł Morawczyński <burciak01@gmail.com>
2025-05-21 21:00:17 +0200

    fix: multiple arguments pass in params fixed

--- shared/__init__.py
+++ shared/__init__.py
@@ -1,3 +1,4 @@
 from .permissions import IsWorker, IsTechnician
 from .validators import not_greater_than_current_year, no_only_digits
 from .enums import RoleEnum
+from .filters import FilterMultipleMixin
