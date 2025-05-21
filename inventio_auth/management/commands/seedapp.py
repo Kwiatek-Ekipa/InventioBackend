c7cec265a21f36ca7887dcaa83e05911b7294cd7
Klusio19 <kusal.jakub@wp.pl>
2025-05-21 22:40:06 +0200

    feat: Created one main seeder for whole app.

---
+++ inventio_auth/management/commands/seedapp.py
@@ -0,0 +1,18 @@
+from django.core.management import BaseCommand, call_command
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--dev",
+            action="store_true"
+        )
+    def handle(self, *args, **options):
+        call_command("seedroles")
+        if options["dev"]:
+            call_command("seedworkers")
+            call_command("seedtechnicians", "--dev")
+            call_command("seedhardware")
+            call_command("seedstocktaking")
+        else:
+            call_command("seedtechnicians")
