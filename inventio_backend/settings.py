c8f0b2cbc4a1953f686251ddacfbf4e2dfe23c6a
Paweł Morawczyński <burciak01@gmail.com>
2025-06-12 02:15:29 +0200

    ref: replace `config` calls with `os.environ` for settings configuration

--- inventio_backend/settings.py
+++ inventio_backend/settings.py
@@ -91,10 +91,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         "NAME": os.environ.get("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("DB_HOST"),
-        "PORT": config("DB_PORT"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
     }
 }
 
@@ -157,8 +157,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=config('ACCESS_TOKEN_LIFETIME_MINUTES', cast=int)),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=config('REFRESH_TOKEN_LIFETIME_DAYS', cast=int)),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=int(os.environ.get('ACCESS_TOKEN_LIFETIME_MINUTES'))),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=int(os.environ.get('REFRESH_TOKEN_LIFETIME_DAYS'))),
 
 }
 
@@ -189,11 +189,11 @@ SPECTACULAR_SETTINGS = {
 
 AUTH_USER_MODEL = 'inventio_auth.Account'
 
-SEED_TECHNICIAN1_EMAIL = config("SEED_TECHNICIAN1_EMAIL")
-SEED_TECHNICIAN1_PASSWORD = config("SEED_TECHNICIAN1_PASSWORD")
-SEED_TECHNICIAN2_EMAIL = config("SEED_TECHNICIAN2_EMAIL")
-SEED_TECHNICIAN2_PASSWORD = config("SEED_TECHNICIAN2_PASSWORD")
-SEED_WORKER1_EMAIL = config("SEED_WORKER1_EMAIL")
-SEED_WORKER1_PASSWORD = config("SEED_WORKER1_PASSWORD")
-SEED_WORKER2_EMAIL = config("SEED_WORKER2_EMAIL")
-SEED_WORKER2_PASSWORD = config("SEED_WORKER2_PASSWORD")
+SEED_TECHNICIAN1_EMAIL = os.environ.get("SEED_TECHNICIAN1_EMAIL")
+SEED_TECHNICIAN1_PASSWORD = os.environ.get("SEED_TECHNICIAN1_PASSWORD")
+SEED_TECHNICIAN2_EMAIL = os.environ.get("SEED_TECHNICIAN2_EMAIL")
+SEED_TECHNICIAN2_PASSWORD = os.environ.get("SEED_TECHNICIAN2_PASSWORD")
+SEED_WORKER1_EMAIL = os.environ.get("SEED_WORKER1_EMAIL")
+SEED_WORKER1_PASSWORD = os.environ.get("SEED_WORKER1_PASSWORD")
+SEED_WORKER2_EMAIL = os.environ.get("SEED_WORKER2_EMAIL")
+SEED_WORKER2_PASSWORD = os.environ.get("SEED_WORKER2_PASSWORD")
